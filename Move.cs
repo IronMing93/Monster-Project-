8f4ee3982252f20271229029dd5dad03f66c3490
IronMing93 <33774207+IronMing93@users.noreply.github.com>
2017-11-20 20:04:43 -0600

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 despriedPostion = target.position + offset;
+        Vector3 smoothPostion = Vector3.Lerp(transform.position, despriedPostion,);
+        
+        transform.position = smoothPostion ;
+        transform.LookAt(target);
+    }
+
+	
+}
